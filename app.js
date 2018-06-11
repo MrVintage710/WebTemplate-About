67e8c705ea798cf9514972a9e68aff32f99bfeea
Brooks <brookspalin710@gmail.com>
2018-06-11 17:32:46 -0600

    more edits have been mad, making the template more reliable.

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'))
 
+const shouldAB = false
+var siteA = true
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    if(shouldAB) {
+        if(siteA) {
+            res.sendFile(path.join(__dirname + '/index-a.html'));
+        } else {
+            res.sendFile(path.join(__dirname + '/index-b.html'));
+        }
+
+        siteA = !siteA
+    } else {
+        res.sendfile(path.join(__dirname + "/index-a.html"))
+    }
 })
 
 const port = process.env.PORT || 3000
