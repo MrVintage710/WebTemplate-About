425e4355f423995c01df708b2d2e3aeca40f5cde
Brooks Palin <brookspalin710@gmail.com>
2018-08-10 19:10:26 -0600

    update (8/10/18)

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const Joi = require("joi")
 const path = require("path")
 const fs = require("fs")
 
@@ -25,11 +24,14 @@ app.get("/", (req, res) => {
 })
 
 app.get("/examples/:id", (req, res) => {
-    var file = fs.readFile(path.join(__dirname + "/pdf_" + req.params.id + ".html"), function (err, data) {
+    fs.readFile(path.join(__dirname + "/public/pdf/" + req.params.id + ".pdf"), function (err, data) {
         if (err) {
             res.status(404).send("Cannot find file.")
+            res.end
         } else {
-            res.sendFile(path.join(__dirname + "/pdf_" + req.params.id + ".html"))
+            res.writeHead(200, {'Content-Type': 'application/pdf'})
+            res.write(data)
+            res.end()
         }
     });
 })
